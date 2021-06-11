8e29d33ab89085eee0c45c44d1ce54f1b24a21ec
Matt <48222937+newtinn@users.noreply.github.com>
2021-06-11 13:58:04 +0100

    Add files via upload

---
+++ movie-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import GuessActor from './Components/GuessActor/GuessActor';
+
+function App() {
+  return (
+    <div style={{paddingLeft: "20px"}}>
+      <h1>Movie Quiz App</h1>
+      <GuessActor />
+    </div>
+  );
+}
+
+export default App;
