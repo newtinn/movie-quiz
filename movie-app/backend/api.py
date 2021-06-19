75a1a5fbb75a25705940a25a091311c8e415ca1f
newtinn <newtinn@hotmail.com>
2021-06-19 16:31:02 +0100

    heroku 4

--- movie-app/backend/api.py
+++ movie-app/backend/api.py
@@ -4,7 +4,7 @@ from flask_cors import CORS, cross_origin
 import imdb
 import random
 
-app = Flask(__name__, static_folder='../build', static_url_path='')
+app = Flask(__name__, static_folder='../build', static_url_path='/')
 CORS(app)
 
 ia = imdb.IMDb()
@@ -104,10 +104,9 @@ def GuessMovie():
             'actor1ID': personOneID, 'actor2ID': personTwoID, 'movieID': movie.movieID, 
             'genre': genre, 'year': year}
 
-@app.route('/')
-@cross_origin()
-def serve():
-    return send_from_directory(app.static_folder, 'index.html')
+@app.route("/")
+def index():
+    return app.send_static_file('index.html')
 
 if (__name__ == "__main__"):
     removeDuplicateLines()
